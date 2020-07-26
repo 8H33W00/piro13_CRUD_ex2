6c52f417ae48741bc47f63edd1462904b6671069
8H33W00 <tigerson95@gmail.com>
2020-07-26 15:50:30 +0900

    CRUD 구현

--- diary/article/urls.py
+++ diary/article/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
-from article.views import index, create
+from article.views import index, create, retrieve, update, delete
+
+app_name = 'article'
 
 urlpatterns = [
-    path('', index),
-    path('create/', create),
+    path('', index, name='list'),
+    path('create/', create, name='create'),
+    path('article/<int:pk>/', retrieve, name='retrieve'),
+    path('update/<int:pk>/', update, name='update'),
+    path('article/<int:pk>/delete/', delete, name='delete'),
 ]
--- diary/article/views.py
+++ diary/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from article.models import Article
 
 
@@ -12,4 +12,55 @@ def index(request):
 
 
 def create(request):
-    return render(request, 'article/create.html', context={})
+    # GET
+    if request.method == 'GET':
+        return render(request, 'article/create.html', context={})
+
+    # POST
+    title = request.POST['title']
+    content = request.POST['content']
+    article = Article.objects.create(title=title, content=content)
+
+    pk = article.id
+    url = reverse('article:retrieve', kwargs={'pk': pk})
+    return redirect(to=url)
+
+
+def retrieve(request, pk):
+    article = Article.objects.get(id=pk)
+
+    context = {
+        'article': article
+    }
+
+    return render(request, 'article/retrieve.html', context=context)
+
+
+def update(request, pk):
+    article = Article.objects.get(id=pk)
+
+    # GET
+    if request.method == 'GET':
+        context = {
+            'article': article
+        }
+        return render(request, 'article/update.html', context=context)
+
+    # request에서 받아온 내용들
+    title = request.POST['title']
+    content = request.POST['content']
+    # 데이터 베이스에 바꿀 내용들
+    article.title = title
+    article.content = content
+    article.save()
+
+    url = reverse('article:retrieve', kwargs={'pk': pk})
+    return redirect(to=url)
+
+
+def delete(request, pk):
+    article = Article.objects.get(id=pk)
+    article.delete()
+
+    url = reverse('article:list')
+    return redirect(to=url)
